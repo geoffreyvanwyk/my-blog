86d2da8f7626f23ed0495dcb8af067f0b90b2c77
Geoffrey van Wyk <geoffrey@vanwyk.biz>
2021-04-05 12:04:17 +0200

    style: Add circuitboard background to site pages
    Also:
    * Add padding on all sides of pages.

--- web/themes/custom/geoffreyvanwykdev/tailwind.config.js
+++ web/themes/custom/geoffreyvanwykdev/tailwind.config.js
@@ -2,11 +2,18 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    heroPatterns: {
+      circuitboard: patterns.circuitboard
+    },
+    heroPatternsShades: ["400"],
+    heroPatternsColors: ["gray"],
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+      require('tailwindcss-hero-patterns'),
+  ],
   prefix: 'tw-'
 }
